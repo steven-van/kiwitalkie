65fd98f9565b14fa54095749df92f80089702db5
steven-van <64859587+steven-van@users.noreply.github.com>
2022-02-07 11:41:18 +0100

    fix: message max width

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,15 +3,18 @@ import styled from "styled-components";
 
 const MessageContainer = styled.div`
   padding: 12px;
-  max-width: 30%;
+  max-width: 40%;
   overflow-wrap: break-word;
   font-weight: 500;
   color: white;
   background-color: ${(props) => (props.fromMe ? "#32de84" : "#1d2437")};
-  margin-top: 8px;
+  margin-top: 10px;
   border-radius: 10px;
   align-self: ${(props) => (props.fromMe ? "flex-end" : "flex-start")};
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  @media (max-width: 768px) {
+    max-width: 60%;
+  }
 `;
 
 const Message = ({ children, fromMe }) => {
