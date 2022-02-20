5704978322ca651c8fbf07145d3375121150356b
steven-van <64859587+steven-van@users.noreply.github.com>
2022-02-20 19:05:01 +0100

    fix: timestamp username position and pages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,36 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  align-self: ${(props) => (props.fromMe ? "flex-end" : "flex-start")};
+  max-width: 40%;
+  display: flex;
+  flex-direction: column;
+`;
 const MessageContainer = styled.div`
   padding: 12px;
-  max-width: 40%;
   overflow-wrap: break-word;
   font-weight: 500;
   color: white;
   background-color: ${(props) => (props.fromMe ? "#32de84" : "#1d2437")};
-  margin-top: 10px;
+  margin-top: 2px;
   border-radius: 10px;
-  align-self: ${(props) => (props.fromMe ? "flex-end" : "flex-start")};
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   @media (max-width: 768px) {
     max-width: 60%;
   }
 `;
+const Span = styled.span`
+  color: #373e53;
+  align-self: ${(props) => props.align || "flex-start"};
+`;
 
 const Message = ({ children, fromMe, timestamp }) => {
   return (
-    <>
+    <Container fromMe={fromMe}>
+      {!fromMe && <Span>Hello</Span>}
       <MessageContainer fromMe={fromMe}>{children}</MessageContainer>
-      <span
-        style={{
-          color: "#373e53",
-          marginTop: "2px",
-          alignSelf: fromMe ? "flex-end" : "flex-start",
-        }}
-      >
-        {timestamp}
-      </span>
-    </>
+      <Span align={"flex-end"}>{timestamp}</Span>
+    </Container>
   );
 };
 
---
+++ src/pages/Chatroom.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import Header from "components/Header";
+import styled from "styled-components";
+import InputChat from "components/InputChat";
+import { ToastContainer, toast } from "react-toastify";
+import MessageList from "components/MessageList";
+import InputRoom from "components/InputRoom";
+import "react-toastify/dist/ReactToastify.css";
+
+import io from "socket.io-client";
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: auto;
+  max-width: 1920px;
+  height: 100vh;
+  background-color: #4b4f59;
+`;
+
+const ChatSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+const Sidebar = styled.div`
+  padding: 10px;
+  width: 20%;
+  display: flex;
+  justify-content: center;
+  background-color: #1d2437;
+  @media (max-width: 768px) {
+    width: 30%;
+  }
+`;
+
+const socket = io.connect("http://localhost:1337");
+
+const Chatroom = () => {
+  const [messageList, setMessageList] = useState([]);
+  const [room, setRoom] = useState("");
+
+  const handleJoinRoom = (newRoom) => {
+    if (newRoom !== room) {
+      socket.emit("join-room", newRoom, room);
+      toast.info(`Joined room #${newRoom}`, {
+        theme: "colored",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setRoom(newRoom);
+    }
+  };
+
+  const handleMessageSubmit = (msg) => {
+    if (msg.text.trim() !== "") {
+      socket.emit("send-message", msg, room);
+      const date = new Date();
+      const time =
+        date.getHours().toString().padStart(2, "0") +
+        ":" +
+        date.getMinutes().toString().padStart(2, "0");
+      setMessageList([
+        ...messageList,
+        {
+          text: msg.text,
+          timestamp: time,
+          fromMe: true,
+        },
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive-message", (msg) => {
+      setMessageList([
+        ...messageList,
+        {
+          text: msg.text,
+          timestamp: msg.timestamp,
+          fromMe: false,
+        },
+      ]);
+    });
+  });
+
+  return (
+    <Page>
+      <Sidebar>
+        <InputRoom handleJoinRoom={handleJoinRoom} />
+      </Sidebar>
+      <ChatSection>
+        <Header />
+        <MessageList room={room} messages={messageList} />
+        <InputChat handleMessageSubmit={handleMessageSubmit} />
+      </ChatSection>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Page>
+  );
+};
+
+export default Chatroom;
